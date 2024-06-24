8a4756753b1c921dcfb82244040b63d2bfda9902
Nin-Onin <oninaustria100@gmail.com>
2024-06-25 07:33:21 +0800

    Update contact-edit-profile-information-form.blade.php
    Edit Details -> Information

--- resources/views/pages/contact/partials/contact-edit-profile-information-form.blade.php
+++ resources/views/pages/contact/partials/contact-edit-profile-information-form.blade.php
@@ -5,7 +5,7 @@
         </h2>
 
         <p class="mt-1 text-sm text-gray-600 dark:text-gray-400">
-            {{ __('Edit Contact Details') }}
+            {{ __('Edit Contact Infomation') }}
         </p>
     </header>
 
