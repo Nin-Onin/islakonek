7140552b8bcea2c370184835c22a0a0b702bace4
Nin-Onin <oninaustria100@gmail.com>
2024-07-09 17:15:27 +0800

    Change Edit and Delete to Icon Button

--- resources/views/pages/contact/index.blade.php
+++ resources/views/pages/contact/index.blade.php
@@ -90,8 +90,13 @@
                         @foreach ($contacts as $contact)
                             <tr>
                                 <td
-                                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100">
-                                    {{ $contact->name }}</td>
+                                    class="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900 dark:text-gray-100 flex items-center">
+                                    <img class="object-cover w-10 h-10 rounded-full mr-4"
+                                        src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=880&q=80"
+                                        alt="">
+                                    <div>{{ $contact->name }}</div>
+                                    {{ $contact->name }}
+                                </td>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
                                     {{ $contact->age }}</td>
                                 <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-500 dark:text-gray-300">
@@ -113,7 +118,13 @@
                                             <button
                                                 class="bg-green-500 hover:bg-green-700 text-white font-bold
                                                 rounded py-2 px-4 ">
-                                                <a href="{{ route('contact.edit', ['contact' => $contact->id]) }}">Edit
+                                                <a href="{{ route('contact.edit', ['contact' => $contact->id]) }}">
+                                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                        class="w-5 h-5">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
+                                                    </svg>
                                                 </a>
                                             </button>
 
@@ -124,7 +135,13 @@
                                                 @csrf
                                                 @method('delete')
                                                 <button type="submit"
-                                                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Delete</button>
+                                                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"><svg
+                                                        xmlns="http://www.w3.org/2000/svg" fill="none"
+                                                        viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor"
+                                                        class="w-5 h-5">
+                                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                                    </svg></button>
                                             </form>
                                         </div>
                                     </div>
@@ -137,6 +154,10 @@
         </div>
 
     </div>
+
+
+
+
 
 
     <script>
